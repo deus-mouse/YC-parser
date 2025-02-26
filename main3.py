7b1fd7d217f8327ae760eb5b59c70b6236a8d559
rvsmulskiy <rvsmulskiy@avito.ru>
2025-02-26 19:36:37 +0300

    ++

--- main3.py
+++ main3.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time  # МОД: Для паузы, можно убрать после отладки
+import time  # МОД: Для паузы
 
 # МОД: Инициализация драйвера (убедитесь, что chromedriver доступен в PATH)
 driver = webdriver.Chrome()
@@ -11,10 +11,11 @@ driver = webdriver.Chrome()
 # МОД: Открытие сайта
 driver.get("https://n82183.yclients.com/")
 
-# МОД: Ожидание появления элементов выбора города
+# МОД: Ожидание появления элементов кнопок выбора города по альтернативному селектору
 wait = WebDriverWait(driver, 10)
-# МОД: Предполагается, что список городов представлен ссылками внутри контейнера с классом "cities"
-cities = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".cities a")))
+cities = wait.until(EC.presence_of_all_elements_located(
+    (By.XPATH, "//a[contains(@href, 'select-city')]")  # МОД: Изменён селектор для выбора города
+))
 
 if cities:
     first_city = cities[0]
