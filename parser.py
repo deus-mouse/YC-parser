e55c3716c4fac5e43d5394012908fc31c91a6ab5
Roman Smulskiy <rvsmulskiy@avito.ru>
2025-07-30 11:36:06 +0300

    ++

--- parser.py
+++ parser.py
@@ -218,7 +218,6 @@ class YCParser:
     def upsert_branches_dict(self, master_name, branch_name):
         self.branches[branch_name] += self.masters[master_name]
 
-
     def quit(self):
         time.sleep(3)
         self.driver.quit()
