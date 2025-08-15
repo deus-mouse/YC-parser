aabdf53d5aed0547fc599631b061d6226adc8cd2
rvsmulskiy <rvsmulskiy@avito.ru>
2025-08-15 21:43:31 +0300

    ++

--- handler.py
+++ handler.py
@@ -8,12 +8,15 @@ from parser import YCParser
 
 
 class Handler:
-    def __init__(self, st, url):
+    def __init__(self, st, url, today, depth_days):
         self.st = st
         self.url = url
         self.city = None
         self.driver = webdriver.Chrome()
         self.wait = WebDriverWait(self.driver, 5)
+        self.parser = None
+        self.today = today
+        self.depth_days = depth_days
 
     def open_site(self, url=None):
         if url:
@@ -46,6 +49,61 @@ class Handler:
 
     def start_from_city(self):
         self.parser = YCParser(url=self.url, city=self.city, st=self.st)
+        self.parser.open_site()
+        self.parser.choose_city()
+        br_count = len(self.parser.find_branches())
+        print("Видим филиалов:", br_count)
+
+        for br in range(br_count):
+            branch_buttons = self.parser.find_branches()
+            branch = branch_buttons[br]
+            branch_name = branch.text.strip()
+            print(f'Выбираем филиал: {branch_name}')
+
+            self.parser.choose_branch(branch)  # откатываемся к этой странице филиалов
+            self.parser.choose_individual_services()
+            self.parser.choose_specialist()
+            self.run_from_masters(branch_name)
+            break
+
+        self.parser.quit()
+        print(f'{self.parser.branches = }')
+        print(f'{self.parser.masters = }')
+
+    def start_from_masters(self):
+        self.parser = YCParser(url=self.url, st=self.st)
+
+    def run_from_masters(self, branch_name=None):
+        master_buttons = self.parser.find_masters()
+        m_count = len(master_buttons)
+        print("Видим мастеров:", m_count - 1)
+
+        while len(self.parser.masters) < m_count - 1:  # 0 = "Любой специалист"
+            # while len(parser.masters) < 2:  # 0 = "Любой специалист"
+            print(f'--------> посчитано мастеров {len(self.parser.masters)}')
+            master_buttons = self.parser.find_masters()
+            master = next((master for master in master_buttons if
+                           master.text.strip() not in self.parser.masters and master.text.strip() != "Любой специалист"),
+                          None)
+            master_name = master.text.strip()
+            print(f'{master_name = }')
+
+            self.parser.choose_master(master)  # страница мастеров
+            self.parser.choose_service_page()
+            min_time = self.parser.select_min_service()
+            self.parser.choose_date_and_time()
+            self.parser.check_working_days(self.today, self.depth_days, master_name, min_time, branch_name)
+            self.parser.upsert_branches_dict(master_name, branch_name)
+
+            self.parser.go_back(self.parser.depths['master'])  # откатываемся к странице мастеров
+            # parser.go_back(parser.depth['service'])
+            # parser.go_back(parser.depth['date_and_time'])
+            # parser.go_back(parser.depth['service_page'])
+            print(f'{self.parser.branches = }')
+            print(f'{self.parser.masters = }')
+            # break  # todo remove
+        # parser.go_back(parser.depth['branch'])
+
 
 
     def pause(self):
