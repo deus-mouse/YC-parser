92b30eb95850f222065129e6585230bac09c9eb4
rvsmulskiy <rvsmulskiy@avito.ru>
2025-10-31 10:32:04 +0300

    ++

--- new/enter_point.py
+++ new/enter_point.py
@@ -1,12 +1,16 @@
 from handler import Handler
-import time
 
 URL_city = "https://n82183.yclients.com/"
 URL_masters_Zvezdinka = 'https://n248723.yclients.com/company/25809/personal/select-master?o=m739791'
 URL_masters_Belorusskaya = 'https://n625088.yclients.com/company/266762/personal/select-master?o='
-CITY = 'Москва'
+
 depth_days = 30  # глубина сканирования
 
 
 def main():
     handler = Handler(freeze=1, url=URL_masters_Zvezdinka, depth=depth_days)
+    handler.run()
+
+
+if __name__ == "__main__":
+    main()
