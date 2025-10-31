0719993fb504d9f7e53367604d25a34c8ae0f159
rvsmulskiy <rvsmulskiy@avito.ru>
2025-10-31 10:15:46 +0300

    ++

---
+++ new/enter_point.py
@@ -0,0 +1,12 @@
+from handler import Handler
+import time
+from datetime import datetime
+
+URL_city = "https://n82183.yclients.com/"
+URL_masters = 'https://n248723.yclients.com/company/25809/personal/select-master?o=m739791'
+URL_masters_Belorusskaya = 'https://n625088.yclients.com/company/266762/personal/select-master?o='
+CITY = 'Москва'
+depth_days = 30  # глубина сканирования
+today = datetime.now()
+
+
